8ebea5cc5dcc74c4a915d8a22202d3a135301a61
My <penghui327@163.com>
2017-10-13 11:55:40 +0800

    11111

--- app/src/main/java/com/hnrheducation/teachermaintain/activity/EquipmentActivity.java
+++ app/src/main/java/com/hnrheducation/teachermaintain/activity/EquipmentActivity.java
@@ -14,6 +14,7 @@ public class EquipmentActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipment);
+
         initView();
     }
 
